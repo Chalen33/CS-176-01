3210a9d2524569fc431b72a52c95817c92a846a0
Chalen33 <98927158+Chalen33@users.noreply.github.com>
2023-02-09 19:48:20 -0500

    Microwave and Microwave Tester

---
+++ Microwave.java/MicrowaveTester.java
@@ -0,0 +1,21 @@
+
+public class MicrowaveTester {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	Microwave microwaveButton = new Microwave();
+	
+	microwaveButton.time();
+	microwaveButton.time();
+	microwaveButton.power();
+	microwaveButton.start();
+	microwaveButton.reset();
+	
+		
+		
+		
+		
+	}
+
+}
